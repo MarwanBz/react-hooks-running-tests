0f30fc5acf8b572ee109aece2342e97df20076af
MarwanBz <69892218+MarwanBz@users.noreply.github.com>
2023-03-12 11:56:23 +0300

    assignment completed

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,10 @@ import React from "react";
 
 import Article from "../components/Article";
 
-test("displays the text 'please pass this test'", () => {
+test("displays the text 'please pass this test for me'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  screen.debug();
+
+  expect(screen.queryByText("please pass this test for me")).toBeInTheDocument();
 });
